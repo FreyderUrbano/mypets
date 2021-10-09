8dffc425979ca2b5b97eaa004afb8b057e3a2a43
FreyderUrbano <freurbano@gmail.com>
2021-10-09 15:02:22 -0500

    update

--- Mypets/settings.py
+++ Mypets/settings.py
@@ -36,8 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
-    'gestion.apps.GestionConfig'
+    'django.contrib.staticfiles',
+    'gestion.apps.GestionConfig',
     'tienda.apps.TiendaConfig',
 ]
 
--- tienda/models.py
+++ tienda/models.py
@@ -3,15 +3,87 @@ from django.db.models.fields import BooleanField, DateTimeField
 
 # Create your models here.
 class user(models.Model):
-    id = Integer(primary_key=True)
-    first_name = VARCHAR(128)
-    last_name = VARCHAR(128)
-    id_identification = Integer
-    number_id = VARCHAR(15)
-    id_city = Integer(15)
-    email = VARCHAR(200)
-    password = VARCHAR(200)
-    status = BooleanField
-    created_at = DateTimeField
-    updated_at = DateTimeField
-    deleted_at = DateTimeField
+    id = models.IntegerField.primary_key = True
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=128)
+    id_identification_type = models.IntegerField
+    id_city = models.IntegerField
+    email = models.CharField(max_length = 200)
+    password = models.CharField(max_length = 200)
+    status = models.BooleanField
+    created_at = models.DateTimeField
+    updated_at = models.DateTimeField
+    deleted_at = models.DateTimeField
+
+class Session(models.Model):
+    id = models.IntegerField.primary_key = True
+    id_user = models.IntegerField
+    ip = models.CharField(max_length = 200)
+    status = models.BooleanField
+    created_at = models.DateTimeField
+    updated_at = models.DateTimeField
+    deleted_at = models.DateTimeField
+
+class Identification_type(models.Model):
+    id = models.IntegerField(20).primary_key = True
+    type = models.CharField(max_length = 150)
+    abrow = models.CharField(max_length = 4)
+    created_at = models.DateTimeField
+    updated_at = models.DateTimeField
+    deleted_at = models.DateTimeField
+
+class City(models.Model):
+    id = models.IntegerField
+    code = models.CharField(max_length = 10)
+    name = models.CharField(max_length = 150)
+    abrev = models.CharField(max_length = 4)
+    id_country = models.IntegerField
+    status = models.BooleanField
+    created_at = models.DateTimeField
+    updated_at = models.DateTimeField
+    deleted_at = models.DateTimeField
+
+class Country(models.Model):
+    id = models.IntegerField
+    code = models.CharField(max_length = 10)
+    name = models.CharField(max_length = 150)
+    abrev = models.CharField(max_length = 4)
+    status = models.BooleanField
+    created_at = models.DateTimeField
+    updated_at = models.DateTimeField
+    deleted_at = models.DateTimeField
+
+class Pet(models.Model):
+    id = models.IntegerField
+    code = models.CharField(max_length = 10)
+    name = models.CharField(max_length = 150)
+    status = models.BooleanField
+    id_user = models.IntegerField
+    id_type = models.IntegerField
+    id_race = models.IntegerField
+    created_at = models.DateTimeField
+    updated_at = models.DateTimeField
+    deleted_at = models.DateTimeField
+
+class Type(models.Model):
+    id = models.IntegerField
+    code = models.CharField(max_length = 100)
+    name = models.CharField(max_length = 150)
+    abrev = models.CharField(max_length = 4)
+    status = models.BooleanField
+    created_at = models.DateTimeField
+    updated_at = models.DateTimeField
+    deleted_at = models.DateTimeField
+
+class Race(models.Model):
+    id = models.IntegerField
+    code = models.CharField(max_length = 10)
+    name = models.CharField(max_length = 150)
+    abrev = models.CharField(max_length = 4)
+    status = models.BooleanField
+    created_at = models.DateTimeField
+    updated_at = models.DateTimeField
+    deleted_at = models.DateTimeField
+
+
+
